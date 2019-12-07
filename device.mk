78255da65c51807a6f8f842c038d0df99c76c532
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-07 22:31:24 +0100

    mido: Introduce Safailnet

--- device.mk
+++ device.mk
@@ -305,7 +305,8 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.safailnet.rc
 
 # RCS
 PRODUCT_PACKAGES += \
